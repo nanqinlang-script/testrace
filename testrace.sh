94b24850affa6b0ee2316795b97dfc4abdb6c691
小亦 <123895181+insoyi@users.noreply.github.com>
2023-02-07 15:34:43 +0800

    Update testrace.sh

--- testrace.sh
+++ testrace.sh
@@ -9,8 +9,8 @@ echo -e "${Green_font}
 # Project: testrace
 # Version: 1.2
 # Author: nanqinlang
-# Blog:   https://sometimesnaive.org
-# Github: https://github.com/nanqinlang
+# Website: https://api.isoyi.net
+# Github: https://github.com/insoyi
 #======================================
 ${Font_suffix}"
 
